adecb91fbf376a903933b1580f4a9763b9801c2c
Fabian Valle <fabian.valle-simmons@mongodb.com>
2023-05-21 12:22:54 -0400

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -17,7 +17,7 @@ export const scroll = new SmoothScroll('a[href*="#"]', {
   speed: 1000,
   speedAsDuration: true,
 });
-const APP_ID = 'APP_ID_HERE';
+const APP_ID = 'application-0-';
 const ATLAS_SERVICE = 'mongodb-atlas';
 const realmApp = new Realm.App({id: APP_ID});
 
@@ -28,7 +28,7 @@ const App = () => {
     await realmApp.logIn(credentials);
     const mongodb = realmApp.currentUser.mongoClient(ATLAS_SERVICE);
     const websitesColl = mongodb.db("web").collection("sites");
-
+    
     let x = await websitesColl.findOne({"site_id":"demo0"}, {
         "projection": {
             "_id": 1,
